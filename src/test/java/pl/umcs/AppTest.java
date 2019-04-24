2898561113552901afe2403721eee52c22bbaf6a
Marcin <marcin.chec94@wp.pl>
2019-04-24 17:37:56 +0200

    added google guava and joiner, splitter tests

--- src/test/java/pl/umcs/AppTest.java
+++ src/test/java/pl/umcs/AppTest.java
@@ -1,18 +1,35 @@
 package pl.umcs;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldConcatWordsWithDot() {
+        List<String> cars = Lists.newArrayList("Audi", "BMW", "VW", "Peugeot");
+
+        String concatedListWithDot = Joiner.on(".").join(cars);
+
+        assertEquals("Audi.BMW.VW.Peugeot", concatedListWithDot);
+    }
+
+    @Test
+    public void shouldExtractWordsFromStringWithSeparatedWords() {
+
+        String source = "Audi.BMW.VW.Peugeot";
+
+        List<String> cars = Splitter.on(".").splitToList(source);
+
+        assertEquals(cars, Lists.newArrayList("Audi", "BMW", "VW", "Peugeot"));
     }
 }
